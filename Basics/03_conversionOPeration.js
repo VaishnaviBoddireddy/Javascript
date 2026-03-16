5f82ef81837c63a5f803e0fb73f32f392c64ceb8
VaishnaviBoddireddy <boddireddy100052000@gmail.com>
2026-03-16 05:30:00 +0000

    updated

--- Basics/03_conversionOPeration.js
+++ Basics/03_conversionOPeration.js
@@ -1,9 +1,9 @@
 let score=undefined
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
 //"33"---33
 //"33abc"---NaN(Not a Number)
 //"undefined"---NaN
@@ -11,7 +11,20 @@ console.log(valueInNumber)
 let isLoggedIn=1
 //console.log(isLoggedIn)
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 //if value entered---true---1,"value"
 //no value entered---false---0, ""
 //stringConversion
+
+//operations
+let value=3
+let negValue=-value
+console.log(negValue)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+//prefix or postfix js.ndm
+
+//Link to refer the documentation:https://developer.mozilla.org/en-US/docs/Web/JavaScript
+//https://tc39.es/ecma262/
