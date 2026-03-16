218479508ca2431f79c7fee77c11a55c4c7b0f96
VaishnaviBoddireddy <boddireddy100052000@gmail.com>
2026-03-16 04:29:27 +0000

    Conversion

---
+++ Basics/03_conversionOPeration.js
@@ -0,0 +1,17 @@
+let score=undefined
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+//"33"---33
+//"33abc"---NaN(Not a Number)
+//"undefined"---NaN
+//"true"---1; "false"---0
+let isLoggedIn=1
+//console.log(isLoggedIn)
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//if value entered---true---1,"value"
+//no value entered---false---0, ""
+//stringConversion
