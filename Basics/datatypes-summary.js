b333dd1e7d894fb1d7676f372d958a71c81b483a
VaishnaviBoddireddy <boddireddy100052000@gmail.com>
2026-03-17 16:51:34 +0000

    updated datatypes summary

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -1,6 +1,6 @@
 //primitive
 //non primitive/ reference---arrays, objects, functions
-const heros = ["hanuman", "shiva"]
+//const heros = ["hanuman", "shiva"]
 
 
 //let myObj={
@@ -9,9 +9,28 @@ const heros = ["hanuman", "shiva"]
 //}
 
 
- const myFunction=function(){
-    console.log("JavaScript")
- }
+ //const myFunction=function(){
+   // console.log("JavaScript")
+ //}
 //7 types:String, Number, Boolean, Null, Undefined, Symbol, Integer(bigInt)
-const score=100/false/true/"vaishnavi"/undefined/Symbol(value)/10.3/null
-console.log(typeof myFunction);
+//const score=100/false/true/"vaishnavi"/undefined/Symbol(value)/10.3/null
+//console.log(typeof myFunction);
+
+
+//memory types
+//stack(primitive---gets a copy of original), Heap(Non-primitive---gets the reference for original)
+
+let myYoutubename = "Vaishnavi Boddireddy"
+let anothername = myYoutubename
+anothername = "Boddireddy"
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne={
+   emailId: "vaishnavi@google.com",
+   upi: "vaishnavi@ybl"
+}
+let userTwo = userOne
+userTwo.emailId = "boddireddy@google.com"
+console.log(userOne)
+console.log(userTwo.emailId)
