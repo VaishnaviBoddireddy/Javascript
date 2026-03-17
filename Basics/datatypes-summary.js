135ab75727244875d32e6f63e5249195d47187c4
VaishnaviBoddireddy <boddireddy100052000@gmail.com>
2026-03-17 15:03:13 +0000

    datatypes summary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+//primitive
+//non primitive/ reference---arrays, objects, functions
+const heros = ["hanuman", "shiva"]
+
+
+//let myObj={
+    //name: "vaishnavi boddireddy",
+   // age: 25,
+//}
+
+
+ const myFunction=function(){
+    console.log("JavaScript")
+ }
+//7 types:String, Number, Boolean, Null, Undefined, Symbol, Integer(bigInt)
+const score=100/false/true/"vaishnavi"/undefined/Symbol(value)/10.3/null
+console.log(typeof myFunction);
