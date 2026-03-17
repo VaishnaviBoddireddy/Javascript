b35479f2061448c150d63db9771db86fc0124d0a
VaishnaviBoddireddy <boddireddy100052000@gmail.com>
2026-03-17 18:21:57 +0000

    strings

---
+++ Basics/05_strings.js
@@ -0,0 +1,38 @@
+const name = "vaishnavi-boddireddy"
+const repoCount = 50
+//console.log(name + repoCount + "Value")
+console.log(`Hello my name is ${name} and my repo count is {repoCount}`)
+const gameName = new String('Vaishnavi Boddireddy')
+console.log(gameName)
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.charAt(1))
+console.log(gameName.indexOf("v"))
+console.log(gameName.indexOf('V'))
+const newString = gameName.substring(0, 9)
+console.log(newString)
+const anotherString =  gameName.slice(3, 17)
+console.log(anotherString)
+const url = "https://Boddireddy.com/Vaishnavi-Boddireddy"
+console.log(url.replace('-','%20'))
+console.log(url.includes('reddy'))
+console.log(url.includes('Sri'))
+console.log(gameName.split( ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
