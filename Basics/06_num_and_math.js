40512f5133579d0c9e35a56b9851c26647b3f792
VaishnaviBoddireddy <boddireddy100052000@gmail.com>
2026-03-18 06:56:31 +0000

    updated number and maths

--- Basics/06_num_and_math.js
+++ Basics/06_num_and_math.js
@@ -1,5 +1,6 @@
 const score = 300
-console.log(score)
+console.log(score.length)
+console.log(typeof score)
 const balance = new Number(100)
 console.log(balance)
 console.log(balance.constructor)
@@ -9,3 +10,12 @@ console.log(balance.toLocaleString(2))
 console.log(balance.toPrecision(2))
 console.log(balance.toString(2))
 console.log(balance.valueOf(2))
+console.log(balance.length)
+//Maths//
+console.log(Math)
+console.log(Math.acosh(3))//gives angle of the given number
+console.log(Math.sqrt(7))
+console.log(Math.random())//gives the value in between 0 and 1 as result
+//Formula---Math.random()*(max-min+1)
+//floor value---Math.random()*(max-min+1)+min
+//round value---Math.random()*(max-min+1)+/-max
