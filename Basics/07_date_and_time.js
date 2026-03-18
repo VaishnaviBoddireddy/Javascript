9042e9515d7bd879ee7437741f3a720701bad9e6
VaishnaviBoddireddy <boddireddy100052000@gmail.com>
2026-03-18 07:42:40 +0000

    date and time

---
+++ Basics/07_date_and_time.js
@@ -0,0 +1,8 @@
+//Dates
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toJSON())
+console.log(myDate.getSeconds())
+console.log(typeof myDate)
+let myCreatedDate = new Date(2026, 1, 12)
+console.log(myCreatedDate.toLocaleTimeString())
