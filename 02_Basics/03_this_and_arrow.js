f9d1ccd112da6c782205b818abcca2f3a3663d7b
VaishnaviBoddireddy <boddireddy100052000@gmail.com>
2026-03-22 10:19:18 +0000

    this and array

---
+++ 02_Basics/03_this_and_arrow.js
@@ -0,0 +1,32 @@
+const user = {
+    username: "Vaishnavi Boddireddy",
+    password: 123,
+    welcomeMessage: function(){
+        console.log(`${this.username}, use the above password to access the repository`)
+        //console.log(this)
+    }
+}
+//user.welcomeMessage()
+//user.username = "Jashwanth"
+//user.welcomeMessage()
+//console.log(this)
+function Vaishu(){
+    //let user = "Vaishnavi Boddireddy"
+    console.log(this)
+}
+Vaishu()
+const chai = function(){
+    let USERNAME = "Vaishnavi Boddireddy"
+    console.log(this.USERNAME)
+}
+chai()
+const chay = () =>{
+    let USERNAME = "Vaishnai Boddireddy"
+    console.log(this.USERNAME)
+} 
+chay()
+//Arroe Function
+//Implicit return---code written in same line and its gets executed and gives output
+//Explicit return---return keyword is used
+//for curly braces "return" keyword is used---returns the values when mentioned with return keyword
+//for paranthasis no "return" keyword is used---returns the value directly
